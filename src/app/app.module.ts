6365a7ce7ab661ef9f4c4abeec635fe77ccfef9e
Dean Reid <dar362003@yahoo.com>
2020-04-15 12:24:21 -0500

    Additional implements from training

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent} from './products/product-list.component';
+import { ConverToSpacesPipe } from 'src/shared/convert-to-spaces.pipe';
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConverToSpacesPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
