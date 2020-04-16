2318a3786e976ae809e066c5b734002c0b3afd81
Dean Reid <dar362003@yahoo.com>
2020-04-15 21:46:35 -0500

    Http, Observable and subscription

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent} from './products/product-list.component';
@@ -16,7 +17,8 @@ import { StarComponent } from 'src/shared/star.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -2,20 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 
-
-
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.css'],
-    
+    styleUrls: ['./product-list.component.css']    
 })
+
 export class ProductListComponent implements OnInit {
   
     title: string ="Product List";
     imageWidth: number=50;
     imageMargin: number =2;
     showImage: boolean=false;
+    errorMessage: string;
     // listFilter: string='cart';
     _listFilter: string;
     get listFilter(): string {
@@ -32,8 +31,15 @@ export class ProductListComponent implements OnInit {
       this._listFilter = '';      
     }
     ngOnInit(): void {
-     this.products = this.productService.getProducts();
-     this.filteredProducts = this.products;
+      
+     this.productService.getProducts().subscribe({
+       next: products => {
+         this.products = products,
+         this.filteredProducts = this.products;
+       },
+       error: err => this.errorMessage = err
+     });
+    // this.filteredProducts = this.products;
     }
 
      performFilter (filterBy: string): IProduct[] {
