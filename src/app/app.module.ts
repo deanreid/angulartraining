8045a9a2deb39973881eb19b495b22709aa0fd5a
Dean Reid <dar362003@yahoo.com>
2020-04-17 16:42:36 -0500

    Routing implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent} from './products/product-list.component';
 import { ConverToSpacesPipe } from 'src/shared/convert-to-spaces.pipe';
 import { StarComponent } from 'src/shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConverToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id', component:ProductDetailComponent},
+      {path: 'welcome', component:WelcomeComponent},
+      {path: '', redirectTo:'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo:'welcome', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
